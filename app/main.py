a755e480ea57919f59db6e1e91dcc82391fcc7ba
yanchengqian <yanchengqian@yanchengqiandeMacBook-Air.local>
2026-06-21 21:14:58 +0800

    fix: allow file protocol requests

--- app/main.py
+++ app/main.py
@@ -15,6 +15,7 @@ app = FastAPI(title="yanshi backend", version="0.1.0")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
+        "null",
         "http://127.0.0.1:5500",
         "http://localhost:5500",
         "http://127.0.0.1:8001",
